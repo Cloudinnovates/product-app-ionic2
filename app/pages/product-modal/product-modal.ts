4994bb8addcff57fe02477cbe60b7c877ca0e019
Plínio Naves <pliniopjn@hotmail.com>
2016-07-11 20:08:04 -0300

    capitulo08-aula07

---
+++ app/pages/product-modal/product-modal.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the ProductModalPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/product-modal/product-modal.html',
+})
+export class ProductModalPage {
+
+  constructor(private nav: NavController) {
+
+  }
+
+}
