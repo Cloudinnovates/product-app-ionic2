8453c784f414585bae5f27b9ef533703c6613017
Plínio Naves <pliniopjn@hotmail.com>
2016-07-11 20:42:40 -0300

    capitulo08-aula08

--- app/pages/product-modal/product-modal.ts
+++ app/pages/product-modal/product-modal.ts
@@ -1,18 +1,57 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ViewController, NavParams } from 'ionic-angular';
+import { ProductService } from './../../providers/product-service/product-service';
+import { CategoryService } from './../../providers/category-service/category-service';
 
-/*
-  Generated class for the ProductModalPage page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   templateUrl: 'build/pages/product-modal/product-modal.html',
 })
 export class ProductModalPage {
 
-  constructor(private nav: NavController) {
+  product: any;
+  categories: Array<any>;
+
+  constructor(private nav: NavController, private view: ViewController, private params: NavParams, private productService: ProductService, private categoryService: CategoryService) {
+    this.product = params.get('product') || {};
+
+    this.categoryService.findAll()
+      .then((categories: Array<any>) => {
+        this.categories = categories;
+      }, (error) => {
+        this.categories = [];
+        console.log('Erro ao buscar categorias', error);
+      });
+  }
+
+  close() {
+    this.view.dismiss();
+  }
+
+  save() {
+
+    if (this.product.id != undefined) {
+
+      this.productService.update(this.product)
+        .then((res) => {
+          if (res) {
+            this.view.dismiss();
+          }
+        }, (error) => {
+          console.log('Erro ao atualizar produto', error);
+        });
+
+    } else {
+
+      this.productService.insert(this.product)
+        .then((res) => {
+          if (res) {
+            this.view.dismiss();
+          }
+        }, (error) => {
+          console.log('Erro ao atualizar produto', error);
+        });
+
+    }
 
   }
 
