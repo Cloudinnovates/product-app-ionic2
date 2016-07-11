757255185768d6acd0b2478faa651b7de518b778
Plínio Naves <pliniopjn@hotmail.com>
2016-07-11 19:37:52 -0300

    capitulo08-aula05

--- app/pages/category/category.ts
+++ app/pages/category/category.ts
@@ -1,19 +1,61 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Alert, Modal } from 'ionic-angular';
+import { CategoryService } from './../../providers/category-service/category-service';
+import { CategoryModalPage } from './../category-modal/category-modal';
 
-/*
-  Generated class for the CategoryPage page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   templateUrl: 'build/pages/category/category.html',
 })
 export class CategoryPage {
 
-  constructor(private nav: NavController) {
+  categories: Array<any>;
 
+  constructor(private nav: NavController, private categoryService: CategoryService) {
+    this.findAll();
+  }
+
+  findAll() {
+    this.categoryService.findAll()
+      .then((categories: Array<any>) => {
+        this.categories = categories;
+      }, (error) => {
+        console.log('Erro ao listar categorias:', error);
+      })
+  }
+
+  removeCategory(category) {
+    let alert = Alert.create({
+      title: 'Deletar Categoria',
+      message: 'Deseja realmente deletar a categoria \'' + category.nome + '\'?',
+      buttons: [
+        {text: 'Cancelar'},
+        {
+          text: 'Deletar',
+          handler: (data) => {
+            this.categoryService.delete(category.id)
+              .then((res) => {
+                if (res) {
+                  this.findAll();
+                }
+              }, (error) => {
+                console.log('Erro ao deletar categoria', error);
+              });
+          }
+        }
+      ]
+    });
+
+    this.nav.present(alert);
+  }
+
+  addCategory() {
+    let modal = Modal.create(CategoryModalPage);
+
+    modal.onDismiss(() => {
+      this.findAll();
+    });
+
+    this.nav.present(modal);
   }
 
 }
